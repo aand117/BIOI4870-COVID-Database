6974548fb2df9754bbd49fbcabc2f8768885860a
aand117 <146000412+aand117@users.noreply.github.com>
2024-05-07 16:01:32 -0500

    Update create_covid_sample_table.sql

--- code/create_covid_sample_table.sql
+++ code/create_covid_sample_table.sql
@@ -1,8 +1,8 @@
 create table covid_sample(
-  accession_id VARCHAR(16),
-  pango_lineage VARCHAR(10),
-  bioproject_id VARCHAR(15),
-  biosample_id VARCHAR(12),
+	accession_id VARCHAR(16),
+	pango_lineage VARCHAR(10),
+	bioproject_id VARCHAR(15),
+	biosample_id VARCHAR(12),
 	date DATE,
 	primary key(accession_id)
 );
